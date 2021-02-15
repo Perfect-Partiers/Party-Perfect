30457766b3c8b03783f90094db260f1de355d740
Cynthia Wu <cynthia21wu@gmail.com>
2021-02-14 20:22:00 -0800

    Re-styled navbar brand and party detail card

--- client/src/components/MyNavbar/index.js
+++ client/src/components/MyNavbar/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import "./style.css";
 
 function MyNavbar() {
-    return (
-        <Navbar className="justify-content-center" style={{backgroundColor: "#ee6a59"}}>
-            <Container className="container text-light">
-                <h1>Party Perfect</h1>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar style={{ backgroundColor: "#ee6a59" }} id="navbar">
+      <Navbar.Brand className="brand-style text-light">
+        <h1 className="logo">
+          Party Perfect
+          <span>
+            <img
+              className="brand-image"
+              alt="Party hat logo"
+              src={process.env.PUBLIC_URL + "/assets/images/hat.png"}
+            ></img>
+          </span>
+        </h1>
+      </Navbar.Brand>
+    </Navbar>
+  );
 }
 
 export default MyNavbar;
--- client/src/components/PartyDetailCard/index.js
+++ client/src/components/PartyDetailCard/index.js
@@ -1,28 +1,41 @@
 import React from "react";
-import {CardColumns, Card, Col} from "react-bootstrap";
+import { CardColumns, Card, Col } from "react-bootstrap";
+import "./style.css";
 
 const styles = {
   card: {
-      backgroundColor: "#8dc6bf"
+    backgroundColor: "#8dc6bf",
   },
+
+  title: {
+    color: "#ffffff",
+    fontSize: "30px",
+    fontWeight: "bolder",
+  },
+
+  footer: {},
 };
 
 function PartyDetailCard() {
   return (
     <Col>
-    <CardColumns>
-      <Card style={styles.card}>
-        <Card.Body>
-          <Card.Subtitle className="mb-2 text-muted">Party Name</Card.Subtitle>
-          <Card.Text>
-            <h3>Party Date</h3>
-            <h3>Party Time</h3>
-            <h3>Party Location</h3>
-          </Card.Text>
-          <Card.Footer>Attendees: 12</Card.Footer>
-        </Card.Body>
-      </Card>
-    </CardColumns>
+      <CardColumns>
+        <Card style={styles.card}>
+          <Card.Body>
+            <Card.Title style={styles.title} className="mb-2">
+              Party Name
+            </Card.Title>
+            <Card.Text>
+              <h3>Party Date</h3>
+              <h3>Party Time</h3>
+              <h3>Party Location</h3>
+            </Card.Text>
+            <Card.Footer style={styles.footer}>
+              <span className="font-weight-bold">Attendees:</span> 12
+            </Card.Footer>
+          </Card.Body>
+        </Card>
+      </CardColumns>
     </Col>
   );
 }
