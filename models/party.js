447c3c16a9f0e1c7c724275a8a07f05d75fb74a9
jre23 <jre.estrada@gmail.com>
2021-02-16 13:08:46 -0800

    updated seed data with real addresses, updated model to include creater parameter, added comments to parties and users.js route handlers, fixed minor errors to get app running

--- models/party.js
+++ models/party.js
@@ -2,12 +2,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const partySchema = new Schema({
-  name: { type: String, required: true },
-  date: { type: String, required: true },
-  time: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
   address: {
-    street: { type: String, required: true },
-    zip: { type: Number, required: true },
+    street: {
+      type: String,
+      required: true,
+    },
+    zip: {
+      type: Number,
+      required: true,
+    },
   },
   attendees: [
     {
