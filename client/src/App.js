a253d7001961a726fb01d7990f86f8dc83ac6202
Tommy Willen <tommyallen1215@gmail.com>
2021-02-20 13:29:59 -0800

    fixed conflict that the vs code missed!

--- client/src/App.js
+++ client/src/App.js
@@ -13,47 +13,6 @@ import { useAuth } from "./components/contexts/AuthContext";
 import ForgotPassword from "../src/pages/ForgotPassword";
 
 function App() {
-<<<<<<< HEAD
-     const { currentUser } = useAuth();
-     console.log(currentUser);
-     return (
-          <Router>
-               <MyNavbar />
-               {currentUser && (
-                    <Switch>
-                         <Route exact path={["/", "/home"]} component={Home} />
-                         <Route exact path="/party/:id" component={Party} />
-                         <Route
-                              exact
-                              path="/partycreate"
-                              component={PartyCreate}
-                         />
-                    </Switch>
-               )}
-               {!currentUser && (
-                    <Switch>
-                         <Route
-                              exact
-                              path={["/login", "/"]}
-                              component={Login}
-                         />
-                         <Route
-                              exact
-                              path="/create-account"
-                              component={CreateAccount}
-                         />
-                         <Route
-                              path="/forgot-password"
-                              component={ForgotPassword}
-                         />
-                         <Route>
-                              <NoMatch />
-                         </Route>
-                    </Switch>
-               )}
-          </Router>
-     );
-=======
   const { currentUser } = useAuth();
   console.log(currentUser);
   return (
@@ -78,7 +37,6 @@ function App() {
       )}
     </Router>
   );
->>>>>>> 1d1e05dc84acf5fe2e5c134bcb36dd9155d348cc
 }
 
 export default App;
