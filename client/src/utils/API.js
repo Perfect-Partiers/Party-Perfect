e300b67704d9666de0257260d5b2dfa01db9cd0e
Tommy Willen <tommyallen1215@gmail.com>
2021-02-22 19:18:31 -0800

    merge conflicts

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,44 +1,6 @@
 import axios from "axios";
 
 export default {
-<<<<<<< HEAD
-     getParties: function (uid) {
-          return axios.get("/api/parties/all/" + uid);
-     },
-     createParty: function (formObject) {
-          return axios.post("/api/parties/" + formObject.creator, formObject);
-     },
-     getParty: function (id) {
-          return axios.get("/api/parties/" + id);
-     },
-     updateParty: function (id, updates) {
-          return axios.put("/api/parties/" + id, updates);
-     },
-     saveParty: function (id, uid) {
-          console.log("====API.saveParty====");
-          return axios.put("/api/parties/save/" + id + "/" + uid);
-     },
-     deleteParty: function (id, uid) {
-          return axios.delete("/api/parties/" + id + "/" + uid);
-     },
-     updateUser: function (uid) {
-          console.log("====API.updateUser====");
-          console.log(uid);
-          return axios.put("/api/users/" + uid);
-     },
-     checkUser: function (uid) {
-          console.log("====API.checkUser====");
-          console.log(uid);
-          return axios.get("/api/users/check/" + uid);
-     },
-     createUser: function (userData) {
-          console.log("====API.createUser====");
-          return axios.post("/api/users/signup", userData);
-     },
-     getMapBoxData: function (id) {
-          return axios.get("/api/parties/mapbox/" + id);
-     },
-=======
   getParties: function (uid) {
     return axios.get("/api/parties/all/" + uid);
   },
@@ -75,5 +37,4 @@ export default {
   getMapBoxData: function (id) {
     return axios.get("/api/parties/mapbox/" + id);
   },
->>>>>>> 1d1e05dc84acf5fe2e5c134bcb36dd9155d348cc
 };
