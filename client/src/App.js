4f52f726b65a563819bebf23ff28c2a40d087c97
Nirmala <nirmala_ira@hotmail.com>
2021-02-23 18:54:45 -0800

    resolved merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ import { useAuth } from "./components/contexts/AuthContext";
 import ForgotPassword from "../src/pages/ForgotPassword";
 
 function App() {
-<<<<<<< HEAD
      const { currentUser } = useAuth();
      console.log(currentUser);
      return (
@@ -53,32 +52,6 @@ function App() {
                )}
           </Router>
      );
-=======
-  const { currentUser } = useAuth();
-  console.log(currentUser);
-  return (
-    <Router>
-      <MyNavbar />
-      {currentUser && (
-        <Switch>
-          <Route exact path={["/", "/home"]} component={Home} />
-          <Route exact path="/party/:id" component={Party} />
-          <Route exact path="/partycreate" component={PartyCreate} />
-        </Switch>
-      )}
-      {!currentUser && (
-        <Switch>
-          <Route exact path={["/login", "/"]} component={Login} />
-          <Route exact path="/create-account" component={CreateAccount} />
-          <Route path="/forgot-password" component={ForgotPassword} />
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      )}
-    </Router>
-  );
->>>>>>> 1d1e05dc84acf5fe2e5c134bcb36dd9155d348cc
 }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -48,7 +48,6 @@ const styles = {
 };
 
 function Home() {
-<<<<<<< HEAD
      const { currentUser } = useAuth();
      const partyRef = useRef();
      const [show, setShow] = useState(false);
@@ -66,7 +65,6 @@ function Home() {
      const yyyy = today.getFullYear();
 
      today = yyyy + "-" + mm + "-" + dd;
-     console.log(typeof today);
 
      // Use Effect for check user, which in turn calls loadParties
      useEffect(() => {
@@ -93,7 +91,6 @@ function Home() {
 
      // this function uses the currentUser info from firebase (user parameter) and checks if the user is in mongodb. if not, add user to mongodb, then load all of the parties associated with that user
      const checkUser = (user) => {
-          console.log(user.email);
           API.checkUser(user.uid)
                .then((res) => {
                     if (res.data.length === 0) {
@@ -122,18 +119,31 @@ function Home() {
 
           API.saveParty(partyId, currentUser.uid)
                .then((res) => {
-                    console.log("Found the party");
-                    console.log(res.data);
+                    console.log("Added party to your database!");
+                    addAttendee(partyId);
+
                     loadParties();
-                    // Testing 602f11dcae4b1dd724cb55be
+                    handleClose();
                })
                .catch((err) => {
                     console.log(err);
                });
-          handleClose();
      };
 
-     // We need to detemrine what we are doing with this. Is it just being added to state?
+     const addAttendee = (partyId) => {
+          let updates = {
+               attendees: [
+                    { name: currentUser.displayName, email: currentUser.email },
+               ],
+          };
+          API.updateParty(partyId, updates)
+               .then((res) => {
+                    console.log("Added current user as attendee");
+               })
+               .catch((err) => {
+                    console.log(err);
+               });
+     };
 
      return (
           <Container>
@@ -190,7 +200,7 @@ function Home() {
                <Row style={styles.heading}>
                     <h2>Upcoming</h2>
                </Row>
-               <Row>
+               <Row className="justify-content-left">
                     {parties.map((party) => (
                          <PartyDetailCard key={party._id} {...party} />
                     ))}
@@ -198,164 +208,11 @@ function Home() {
                <Row style={styles.heading}>
                     <h2>Past Events</h2>
                </Row>
-               <Row>
+               <Row className="mb-5">
                     <PastAccordion parties={pastParties}></PastAccordion>
                </Row>
           </Container>
      );
-=======
-  const { currentUser } = useAuth();
-  const partyRef = useRef();
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  const [parties, setParties] = useState([]);
-  const [pastParties, setPastParties] = useState([]);
-
-  // Setting today's date
-  let today = new Date();
-  const dd = String(today.getDate()).padStart(2, "0");
-  const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  const yyyy = today.getFullYear();
-
-  today = yyyy + "-" + mm + "-" + dd;
-
-  // Use Effect for check user, which in turn calls loadParties
-  useEffect(() => {
-    checkUser(currentUser);
-  }, []);
-
-  const loadParties = () => {
-    API.getParties(currentUser.uid)
-      .then((res) => {
-        let currentParties = res.data.parties.filter(
-          (party) => party.date >= today
-        );
-        setParties(currentParties);
-
-        let pastParties = res.data.parties.filter(
-          (party) => party.date < today
-        );
-        setPastParties(pastParties);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  // this function uses the currentUser info from firebase (user parameter) and checks if the user is in mongodb. if not, add user to mongodb, then load all of the parties associated with that user
-  const checkUser = (user) => {
-    API.checkUser(user.uid)
-      .then((res) => {
-        if (res.data.length === 0) {
-          console.log("user not in mongodb");
-          API.createUser({
-            email: user.email,
-            uid: user.uid,
-          }).then((results) => {
-            console.log(results);
-          });
-        } else {
-          console.log("user already in mongodb");
-        }
-        loadParties();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const handleAddParty = (e) => {
-    e.preventDefault();
-
-    let partyId = partyRef.current.value;
-    console.log(partyId);
-
-    API.saveParty(partyId, currentUser.uid)
-      .then((res) => {
-        console.log("Added party to your database!");
-        addAttendee(partyId);
-
-        loadParties();
-        handleClose();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const addAttendee = (partyId) => {
-    let updates = {
-      attendees: [{ name: currentUser.displayName, email: currentUser.email }],
-    };
-    API.updateParty(partyId, updates)
-      .then((res) => {
-        console.log("Added current user as attendee");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  return (
-    <Container>
-      <Row className="mt-5 mb-3 justify-content-md-center">
-        <h1>Welcome Perfect Partier {currentUser.displayName}!</h1>
-      </Row>
-      <Row className="justify-content-md-center">
-        <Button href="/partycreate" style={styles.button} className="mr-4">
-          Create a Party
-        </Button>
-        <Button href="#" style={styles.button} onClick={handleShow}>
-          Join a Party
-        </Button>
-
-        <Modal
-          show={show}
-          onHide={handleClose}
-          animation={false}
-          className="text-center"
-        >
-          <Modal.Header closeButton style={styles.modalHead}>
-            <Modal.Title style={styles.modalTitle}>Join a Party</Modal.Title>
-          </Modal.Header>
-          <Modal.Body style={styles.modal} className="font-weight-bold">
-            Enter the party ID below
-            <Form onSubmit={handleAddParty}>
-              <Form.Group>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter Party ID"
-                  style={styles.formControl}
-                  ref={partyRef}
-                ></Form.Control>
-                <Button style={styles.modalButton} type="submit">
-                  Join Party
-                </Button>
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      </Row>
-      <Row style={styles.heading}>
-        <h2>Upcoming</h2>
-      </Row>
-      <Row className="justify-content-left">
-        {parties.map((party) => (
-          <PartyDetailCard key={party._id} {...party} />
-        ))}
-      </Row>
-      <Row style={styles.heading}>
-        <h2>Past Events</h2>
-      </Row>
-      <Row className="mb-5">
-        <PastAccordion parties={pastParties}></PastAccordion>
-      </Row>
-    </Container>
-  );
->>>>>>> 1d1e05dc84acf5fe2e5c134bcb36dd9155d348cc
 }
 
 export default Home;
