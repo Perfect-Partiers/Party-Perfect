64c6adbb0c1a6aba38bd21532b216bc1713f7027
Nirmala <nirmala_ira@hotmail.com>
2021-02-23 20:09:13 -0800

    merging conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ import { useAuth } from "./components/contexts/AuthContext";
 import ForgotPassword from "../src/pages/ForgotPassword";
 
 function App() {
-<<<<<<< HEAD
      const { currentUser } = useAuth();
      console.log(currentUser);
      return (
@@ -53,32 +52,6 @@ function App() {
                )}
           </Router>
      );
-=======
-  const { currentUser } = useAuth();
-  console.log(currentUser);
-  return (
-    <Router>
-      <MyNavbar />
-      {currentUser && (
-        <Switch>
-          <Route exact path={["/", "/home"]} component={Home} />
-          <Route exact path="/party/:id" component={Party} />
-          <Route exact path="/partycreate" component={PartyCreate} />
-        </Switch>
-      )}
-      {!currentUser && (
-        <Switch>
-          <Route exact path={["/login", "/"]} component={Login} />
-          <Route exact path="/create-account" component={CreateAccount} />
-          <Route path="/forgot-password" component={ForgotPassword} />
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      )}
-    </Router>
-  );
->>>>>>> b3d9a207191097d89fdc5282c580a98d2db0d41f
 }
 
 export default App;
