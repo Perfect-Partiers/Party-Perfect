8781f6e56fca6ec55fb4eb00cb4eb3176f78d254
Tommy Willen <tommyallen1215@gmail.com>
2021-02-25 19:00:53 -0800

    fixed routing when logged in to redirect to home on bad url

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import Party from "./pages/Party";
 import NoMatch from "./pages/NoMatch";
@@ -27,7 +27,7 @@ function App() {
                               component={PartyCreate}
                          />
                          <Route>
-                              <NoMatch />
+                              <Redirect to="/home"/>
                          </Route>
                     </Switch>
                )}
